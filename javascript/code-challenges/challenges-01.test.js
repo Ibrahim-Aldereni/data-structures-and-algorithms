4fecd995a9a08921beecc894b1c952cfa8065672
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-06 21:10:33 +0300

    finish challenge 7

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -137,7 +137,19 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
+  let newArr = [];
+  arr.forEach((item) => {
+    if (item % 3 === 0 && item % 5 === 0) {
+      newArr.push("Fizz Buzz");
+    } else if (item % 5 === 0) {
+      newArr.push("Buzz");
+    } else if (item % 3 === 0) {
+      newArr.push("Fizz");
+    } else {
+      newArr.push(item);
+    }
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -211,7 +223,7 @@ describe("Testing challenge 6", () => {
   });
 });
 
-xdescribe("Testing challenge 7", () => {
+describe("Testing challenge 7", () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test("It should print out messages or numbers", () => {
