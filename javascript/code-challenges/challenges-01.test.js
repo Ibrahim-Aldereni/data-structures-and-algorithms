f2ca6775fce628a06e5f090e05b2460458c2364e
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-06 20:52:07 +0300

    finish challenge 5

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -81,11 +81,17 @@ Return the modified array.
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr, value) => {
-  // Solution code here...
+  arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
+  let newArr = new Array(times);
+  newArr.fill(num);
+
+  newArr.forEach((item) => {
+    callback(arr, item);
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
